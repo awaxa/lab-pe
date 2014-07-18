665fd32a0db918adbf2cd979c30eacfe7cc93c4b
Greg Kitson <greg.kitson@gmail.com>
2014-07-18 11:23:32 -0600

    improve bootstrap_r10k.sh

--- scripts/bootstrap_r10k.sh
+++ scripts/bootstrap_r10k.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-/opt/puppet/bin/puppet module install zack-r10k
-/opt/puppet/bin/puppet apply --exec 'include ::profile::puppet::r10k' --modulepath='/vagrant/control/site:/etc/puppetlabs/puppet/modules:/opt/puppet/share/puppet/modules'
+/opt/puppet/bin/puppet resource package git ensure=present
+/usr/bin/git clone /vagrant/control /tmp/control
+/opt/puppet/bin/gem install r10k --no-rdoc --no-ri -v 1.3.0
+PUPPETFILE=/tmp/control/Puppetfile PUPPETFILE_DIR=/tmp/control/modules /opt/puppet/bin/r10k puppetfile install
+/opt/puppet/bin/puppet apply --exec 'include ::role::puppet::master' --modulepath='/tmp/control/site:/tmp/control/modules:/opt/puppet/share/puppet/modules'
 /opt/puppet/bin/r10k deploy environment -pv
 /opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production nodeclass:add['role::puppet::master'] node:addclass['master','role::puppet::master']
 /opt/puppet/bin/puppet agent --test
