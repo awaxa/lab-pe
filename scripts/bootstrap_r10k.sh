ac5e56e57216122e8af0b9cb2713595eb6576a5e
Greg Kitson <greg.kitson@gmail.com>
2014-09-07 08:43:11 -0700

    classification is now handled by the manifestdir

--- scripts/bootstrap_r10k.sh
+++ scripts/bootstrap_r10k.sh
@@ -6,5 +6,4 @@
 PUPPETFILE=/tmp/control/Puppetfile PUPPETFILE_DIR=/tmp/control/modules /opt/puppet/bin/r10k puppetfile install
 /opt/puppet/bin/puppet apply --exec 'include role::pe::master' --modulepath='/tmp/control/site:/tmp/control/modules:/opt/puppet/share/puppet/modules'
 /opt/puppet/bin/r10k deploy environment -pv
-/opt/puppet/bin/rake -f /opt/puppet/share/puppet-dashboard/Rakefile RAILS_ENV=production nodeclass:add['role::puppet::master'] node:addclass['master','role::puppet::master']
 /opt/puppet/bin/puppet agent --test
