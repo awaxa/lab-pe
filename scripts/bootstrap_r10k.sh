b59c1d6084230dbbe18ddecd851994a2a48f250c
Greg Kitson <greg.kitson@gmail.com>
2014-09-07 09:00:06 -0700

    apply master manifest during bootstrapping

--- scripts/bootstrap_r10k.sh
+++ scripts/bootstrap_r10k.sh
@@ -4,6 +4,6 @@
 /usr/bin/git clone /vagrant/control /tmp/control
 /opt/puppet/bin/gem install r10k --no-rdoc --no-ri -v 1.3.0
 PUPPETFILE=/tmp/control/Puppetfile PUPPETFILE_DIR=/tmp/control/modules /opt/puppet/bin/r10k puppetfile install
-/opt/puppet/bin/puppet apply --exec 'include role::pe::master' --modulepath='/tmp/control/site:/tmp/control/modules:/opt/puppet/share/puppet/modules'
+/opt/puppet/bin/puppet apply /tmp/control/manifests/master.pp --modulepath='/tmp/control/site:/tmp/control/modules:/opt/puppet/share/puppet/modules'
 /opt/puppet/bin/r10k deploy environment -pv
 /opt/puppet/bin/puppet agent --test
